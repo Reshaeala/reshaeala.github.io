15f15fc418fbe476433ba11bf409f5812921a11a
Reshaeala <58646198+Reshaeala@users.noreply.github.com>
2020-02-24 19:55:26 -0600

    Added pictures and pokemon moves

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //console.log($);
 
-
+// const images = ('<img>')
 
 
 $('form').on('submit', (event) => {
@@ -13,11 +13,20 @@ $('form').on('submit', (event) => {
           url:  `https://pokeapi.co/api/v2/pokemon/${userInput}`
           }).then(
             (data) => {
-               //console.log(data);
+               console.log(data);
                 $('.pokieName').html(data.name);
+
+                let $pokieImage = $('<img>').addClass('pokieImage');
+                 $pokieImage.append(data.sprites.front_default)
+                $('.pokemon').append($pokieImage)
+             //console.log(data[0].);
+
+               //console.log(data.abilities[0].ability.name);
+                 //  $('.pokieName').html(pokieAbilities)
+
               //console.log(data[0].);
                 //console.log(data.abilities[0].ability.name);
-                  //  $('.pokieName').html(pokieAbilities)
+
                 for (let i = 0; i < data.abilities.length; i++) {
                     //console.log(data.abilities);
                       let $pokieAbilities =$('<p>').text(data.abilities[i].ability.name);
@@ -33,4 +42,5 @@ $('form').on('submit', (event) => {
             console.log('bad request');
             }
           );
+             $
   })
